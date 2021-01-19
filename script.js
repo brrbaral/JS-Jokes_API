dce7107500d6e91f08a80ef3c31a38af488b92ad
Biswaraj Baral <brr.baral@gmail.com>
2021-01-19 12:22:34 +0545

    fetching random jokes from API

---
+++ script.js
@@ -0,0 +1,40 @@
+const jokes=document.querySelector('#joke');
+const jokeBtn=document.querySelector('#jokeBtn');
+
+//*************USING PROMISES***************
+// const generateJokes = ()=>{
+//     const setHeader={
+//         headers:{
+//             Accept : "application/json"
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com/', setHeader)
+//         .then((res)=>res.json())
+//         .then((data)=>{
+//             jokes.innerHTML=data.joke;
+//         })
+//         .catch((error)=>{
+//         console.log(error);
+//     })
+// }
+
+//***********USING ASYNC-AWAIT*****************
+
+const generateJokes = async ()=> {
+    try {
+        const setHeader = {
+            headers: {
+                Accept: "application/json"
+            }
+        }
+        const res = await fetch('https://icanhazdadjoke.com/', setHeader);
+        const data = await res.json();
+        jokes.innerHTML = data.joke;
+    }catch (err) {
+        console.log(`the error is ${err}`);
+    }
+}
+
+// ****COMMON SECTION FOR BOTH PROMISE AND ASYNC-AWAIT*****
+jokeBtn.addEventListener('click',generateJokes);
+generateJokes(); //BY DEFAULT IT THE USER VISIT FIRST TIME IN THE SITE
